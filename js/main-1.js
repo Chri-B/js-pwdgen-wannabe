5175fc459492726d14e857a9ac0b706a8503bc6d
Christian Beretta <beretta.christian2602@gmail.com>
2020-02-17 17:01:37 +0100

    Completamento es facoltativi 1-2-3

--- js/main-1.js
+++ js/main-1.js
@@ -2,8 +2,11 @@
 var nomeUtente = prompt ('Qual è il tuo nome?');
 var cognomeUtente = prompt ('Qual è il tuo cognome?');
 var coloreUtente = prompt ('Qual è il tuo colore preferito?');
+// variabili per anno corrente
+var data = new Date();
+var annoCorrente = data.getFullYear();
 // calcolo password generata
-var pswGenerata = nomeUtente + cognomeUtente + coloreUtente + 20;
+var pswGenerata = nomeUtente + cognomeUtente + coloreUtente + annoCorrente;
 // display password generata solo dopo compilazione prompt
 document.getElementById('psw-finale').innerHTML = pswGenerata;
 document.getElementById('comparsa').setAttribute('class','visible');
